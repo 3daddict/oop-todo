3da165dd88191f4f9ce92184df9847d8d269ff7b
Michael Salvati <print@3daddict.com>
2019-01-01 11:05:24 -0800

    form submission completed with local storage

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(){
+	formValidation()
 	new GetDate().createTodaysDate();
-	formValidation(); //Form validation
-	new Card(100, 'My First OOP Element!!!', 'Finally I have created an element using OOP and inserted it on the DOM!!!');
-	new Card(200, 'Here is another one', 'OOP is awesome I cant wait to practice more');
+	// new Card(100, 'My First OOP Element!!!', 'Finally I have created an element using OOP and inserted it on the DOM!!!');
+	// new Card(200, 'Here is another one', 'OOP is awesome I cant wait to practice more');
 	// new Card(300, 'Take out the trash', 'Trash pickup is on wednesday and dont forget the recycling').createCard();
 	// new Card(300, 'Feed the pets', 'Dog feed twice daily and Fish feed once per day at 4pm').createCard();
 	// new Card(300, 'Vaccuum the carpets', 'Upstairs carpets 1per week and stairs once per month').createCard();
@@ -16,10 +16,9 @@ let id = 0;
 
 //boostrap 4 form validation
 function formValidation() {
-  window.addEventListener('load', function() {
-    var forms = document.getElementsByClassName('needs-validation');
+    const forms = document.getElementsByClassName('needs-validation');
 
-    var validation = Array.prototype.filter.call(forms, function(form) {
+    let validation = Array.prototype.filter.call(forms, function(form) {
       form.addEventListener('submit', function(event) {
         if (form.checkValidity() === false) {
           event.preventDefault();
@@ -28,20 +27,15 @@ function formValidation() {
         form.classList.add('was-validated');
       }, false);
     });
-  }, false);
+
 }
 
 //Form submit function
 function submitForm() {
-	document.getElementById('submitButton').addEventListener('click', () => {
 		const formTitle = document.querySelector('#formTitle').value;
 		const formDesc = document.querySelector('#formDesc').value;
-		let newCard = new Card(id, formTitle, formDesc);
+		new Card(id, formTitle, formDesc);
 		id++
-		console.log(newCard);
-		return newCard;
-	});
-	var form = document.getElementById("listInput");
-	form.reset();
-	
+		document.getElementById("listInput").reset();
+
 }
