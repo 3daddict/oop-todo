d7efe5d92ed807babf8bdecfcc3b703754a15cb7
Michael Salvati <print@3daddict.com>
2019-01-02 15:31:08 -0800

    backend setup, need to connect form

---
+++ reference.js
@@ -0,0 +1,14 @@
+//NodeJS REST API: Super Simple Setup - Express & NPM (Ep1)
+//https://www.youtube.com/watch?v=F7NVpxxmmgM
+
+//NodeJS REST API: Connecting to MySQL (Ep 2)
+//https://www.youtube.com/watch?v=mLLUqMpf5H0
+
+//NodeJS REST: Serve HTML Files and POST Requests (Ep 3)
+//https://www.youtube.com/watch?v=1qH-3SIFmXw
+
+//NodeJS REST: How to Refactor Code into Multiple Files with Router (Ep 4)
+//https://www.youtube.com/watch?v=TQU6BWtc3yc
+
+//How to Deploy Custom Backend to Cloud using Heroku (Ep 5)
+//https://www.youtube.com/watch?v=w0HAZKxyrf8
